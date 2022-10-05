717aa53ad1e0aeee3894dd501800e89c12d068db
WhiteCrosses <99359429+WhiteCrosses@users.noreply.github.com>
2022-10-05 22:57:28 +0200

    SDL Project Setup

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+int main( int argc, char *argv[] ){
+    
+    SDL_Window *window;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    window = SDL_CreateWindow(
+        "Okno SDL",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (window == NULL) {
+        printf("Nie utworzono okna: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    //setup renderer
+    SDL_Renderer* renderer = NULL;
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    SDL_RenderClear(renderer);
+
+    SDL_Rect r;
+    r.x = 50;
+    r.y = 50;
+    r.w = 50;
+    r.h = 50;
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderFillRect(renderer, &r);
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(5000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
