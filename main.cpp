3690f8438f5f9cf57da0db151094965c217e1474
WhiteCrosses <99359429+WhiteCrosses@users.noreply.github.com>
2022-10-20 15:04:07 +0200

    Modified files

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main( int argc, char *argv[] ){
     SDL_Init(SDL_INIT_EVERYTHING);
 
     Screen window(640,480);
+    window.SDL_SetRenderDrawColor(255,200,228,1);
+    window.clean();
 
     SDL_Delay(5000);
 
