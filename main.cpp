1585e2c6d0bcf44e69601c32174ff25e51bc499a
WhiteCrosses <99359429+WhiteCrosses@users.noreply.github.com>
2022-10-20 14:34:25 +0200

    Modified files

--- main.cpp
+++ main.cpp
@@ -8,24 +8,9 @@ int main( int argc, char *argv[] ){
 
     Screen window(640,480);
 
-    //setup renderer
-
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    SDL_Rect r;
-    r.x = 50;
-    r.y = 50;
-    r.w = 50;
-    r.h = 50;
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-    SDL_RenderFillRect(renderer, &r);
-
-    SDL_RenderPresent(renderer);
     SDL_Delay(5000);
 
-    SDL_DestroyWindow(window);
+    Screen.gameOver();
     SDL_Quit();
 
     return 0;
