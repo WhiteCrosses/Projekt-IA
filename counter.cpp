d99c3ad9f005afaf208620d7d5149618a0a691d8
0keyss <mat.wilk4@gmail.com>
2022-11-28 23:05:52 +0100

    class counter

---
+++ counter.cpp
@@ -0,0 +1,14 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <iostream>
+#include "counter.h"
+#include <cmath>
+
+counter::incrementEnemy(){
+    
+}
+
+counter::reset(){
+    
+}
---
+++ counter.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+
+class Counter{
+    public:
+        int enemyTickCounter;
+        void incrementEnemy();
+        void reset();
+}
